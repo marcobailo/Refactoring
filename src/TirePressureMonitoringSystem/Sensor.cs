5718cfc578929228c1b328764430619e5212447d
marcobailo <bailo.marco@libero.it>
2022-10-13 09:39:04 +0200

    Commit of test, Marco Bailo

--- src/TirePressureMonitoringSystem/Sensor.cs
+++ src/TirePressureMonitoringSystem/Sensor.cs
@@ -1,3 +1,38 @@
+//new source code Bailo Marco
+using System;
+
+namespace TDDMicroExercises.TirePressureMonitoringSystem
+{
+
+    public class Sensor
+    {
+
+        private const double Offset = 16;
+        private readonly Random _randomPressureSampleSimulator;
+
+        public Sensor()
+        {
+            _randomPressureSampleSimulator = new Random();
+        }
+
+        public double PopNextPressurePsiValue()
+        {
+            double _pressureTelemetryValue = ReadPressureSample();
+            return Offset + _pressureTelemetryValue;
+        }
+
+        private double ReadPressureSample()
+        {
+            // Simulate info read from a real sensor in a real tire
+            return 6 * _randomPressureSampleSimulator.NextDouble() * _randomPressureSampleSimulator.NextDouble();
+        }
+
+    }
+
+}
+
+// old source code
+/*
 using System;
 
 namespace TDDMicroExercises.TirePressureMonitoringSystem
@@ -26,3 +61,4 @@ namespace TDDMicroExercises.TirePressureMonitoringSystem
         }
     }
 }
+*/
