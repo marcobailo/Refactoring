dddece8ee64513219fab5bf9138aa8caca137240
marcobailo <bailo.marco@libero.it>
2022-10-13 14:20:20 +0200

    UnicodeFileToHtmlTextConverter  exercise terminated
    Terminated the exercise UnicodeFileToHtmlTextConverter , added a new UnitTest class containing the tests regarding the exercise itself.

---
+++ src/UnicodeFileToHtmlTextConverter.Tests/NUnitFileToHtmlConverter.cs
@@ -0,0 +1,77 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace TDDMicroExercises.UnicodeFileToHtmlTextConverter.Tests
+{
+
+    [TestFixture]
+    class NUnitFileToHtmlConverter
+    {
+
+        private IToHtmlTextConverter _converterFromPlanTextToHtml = null;
+        private string _path;
+        private string _planFileName;
+
+        [SetUp]
+        public void Setup()
+        {
+            _path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            _planFileName = @"\PlanTextFile.txt";
+        }
+
+        [Test]
+        public void ValidateFromTextFile()
+        {
+            _converterFromPlanTextToHtml = new UnicodeFileToHtmlTextConverter(_path + _planFileName);
+            Assert.IsTrue(_converterFromPlanTextToHtml.Validate());
+        }
+
+        [Test]
+        public void ReadFromTextFile()
+        {
+            _converterFromPlanTextToHtml = new UnicodeFileToHtmlTextConverter(_path + _planFileName);
+            string _htmlContent = _converterFromPlanTextToHtml.ConvertToHtml("File title...");
+            Debug.WriteLine("Plan text file content converted into html:");
+            Debug.WriteLine(_htmlContent);
+            Assert.IsTrue(_htmlContent != string.Empty);
+        }
+
+        [Test]
+        public void ValidateFromString()
+        {
+            _converterFromPlanTextToHtml = new StringToHtmlTextConverter(File.OpenText(_path + _planFileName).ReadToEnd());
+            Assert.IsTrue(_converterFromPlanTextToHtml.Validate());
+        }
+
+        [Test]
+        public void ReadFromString()
+        {
+            _converterFromPlanTextToHtml = new StringToHtmlTextConverter(File.OpenText(_path + _planFileName).ReadToEnd());
+            string _htmlContent = _converterFromPlanTextToHtml.ConvertToHtml("File title...");
+            Debug.WriteLine("String converted into html:");
+            Debug.WriteLine(_htmlContent);
+            Assert.IsTrue(_htmlContent != string.Empty);
+        }
+
+        [Test]
+        public void ValidateFromBytesArray()
+        {
+            _converterFromPlanTextToHtml = new ByteArrayToHtmlTextConverter(File.ReadAllBytes(_path + _planFileName));
+            Assert.IsTrue(_converterFromPlanTextToHtml.Validate());
+        }
+
+        [Test]
+        public void ReadFromBytesArray()
+        {
+            _converterFromPlanTextToHtml = new ByteArrayToHtmlTextConverter(File.ReadAllBytes(_path + _planFileName));
+            string _htmlContent = _converterFromPlanTextToHtml.ConvertToHtml("File title...");
+            Debug.WriteLine("String converted into html:");
+            Debug.WriteLine(_htmlContent);
+            Assert.IsTrue(_htmlContent != string.Empty);
+        }
+
+    }
+
+}
--- src/UnicodeFileToHtmlTextConverter/UnicodeFileToHtmlTextConverter.cs
+++ src/UnicodeFileToHtmlTextConverter/UnicodeFileToHtmlTextConverter.cs
@@ -1,4 +1,142 @@
 using System.IO;
+using System.Text;
+using System.Web;
+
+namespace TDDMicroExercises.UnicodeFileToHtmlTextConverter
+{
+
+    public interface IToHtmlTextConverter
+    {
+
+        string ConvertToHtml(string title = null);
+
+        bool Validate();
+
+    }
+
+    public abstract class ToHtmlTextConverterBase : IToHtmlTextConverter
+    {
+
+        public abstract string ConvertToHtml(string title = null);
+
+        public virtual bool Validate() { return false; }
+
+        protected StringBuilder CheckHtmlFromText(StringBuilder text)
+        {
+            text = text.Replace("\r\n", "\r");
+            text = text.Replace("\n", "\r");
+            text = text.Replace("\r", "<br>\r\n");
+            text = text.Replace("  ", " &nbsp;");
+            return text;
+        }
+
+        protected StringBuilder DecorateConvertedPlanTextToHtml(StringBuilder text, string title = null)
+        {
+            if (text != null && text.Length > 0)
+            {
+                string _header = "<!DOCTYPE html><html><head>";
+                if (string.IsNullOrEmpty(title) == false && title.Trim() != "")
+                    _header += "<title>" + title + "</title>";
+                _header += "</head><body>";
+                text.Insert(0, _header);
+                text.Append("</body></html>");
+            }
+            return text;
+        }
+
+    }
+
+    public class UnicodeFileToHtmlTextConverter : ToHtmlTextConverterBase
+    {
+
+        private readonly string _fullFilenameWithPath;
+
+        public UnicodeFileToHtmlTextConverter(string fullFilenameWithPath)
+        {
+            _fullFilenameWithPath = fullFilenameWithPath;
+        }
+
+        public override bool Validate()
+        {
+            bool _validated = false;
+            if (!(string.IsNullOrEmpty(_fullFilenameWithPath) || _fullFilenameWithPath.Trim() == ""))
+            {
+                if (File.Exists(_fullFilenameWithPath))
+                {
+                    if (new FileInfo(_fullFilenameWithPath).Length > 0)
+                        _validated = true;
+                }
+            }
+            return _validated;
+        }
+
+        public override string ConvertToHtml(string title = null)
+        {
+            string _line;
+            TextReader _unicodeFileStream = File.OpenText(_fullFilenameWithPath);
+            StringBuilder _fileContent = new StringBuilder();
+            while ((_line = _unicodeFileStream.ReadLine()) != null)
+            {
+                _fileContent.Append(HttpUtility.HtmlEncode(_line));
+                _fileContent.Append("<br />");
+            }
+            _unicodeFileStream.Close();
+            _unicodeFileStream.Dispose();
+            return DecorateConvertedPlanTextToHtml(CheckHtmlFromText(_fileContent), title).ToString();
+        }
+
+    }
+
+    public class ByteArrayToHtmlTextConverter : ToHtmlTextConverterBase
+    {
+
+        private readonly byte[] _planTextContentAsByteArray;
+
+        public override bool Validate()
+        {
+            return !(_planTextContentAsByteArray == null || _planTextContentAsByteArray.Length == 0);
+        }
+
+        public ByteArrayToHtmlTextConverter(byte[] bytesArray)
+        {
+            _planTextContentAsByteArray = bytesArray;
+        }
+
+        public override string ConvertToHtml(string title = null)
+        {
+            return DecorateConvertedPlanTextToHtml(CheckHtmlFromText(new StringBuilder(Encoding.UTF8.GetString(_planTextContentAsByteArray))), title).ToString();
+        }
+
+    }
+
+    public class StringToHtmlTextConverter : ToHtmlTextConverterBase
+    {
+
+        private readonly string _planText;
+
+        public override bool Validate()
+        {
+            return !(string.IsNullOrEmpty(_planText) || _planText.Trim() == "");
+        }
+
+        public StringToHtmlTextConverter(string planText)
+        {
+            _planText = planText;
+        }
+
+        public override string ConvertToHtml(string title = null)
+        {
+            return DecorateConvertedPlanTextToHtml(CheckHtmlFromText(new StringBuilder(_planText)), title).ToString();
+        }
+
+    }
+
+}
+
+
+// old source code
+/*
+using System.IO;
 using System.Web;
 
 namespace TDDMicroExercises.UnicodeFileToHtmlTextConverter
@@ -32,3 +170,5 @@ namespace TDDMicroExercises.UnicodeFileToHtmlTextConverter
         }
     }
 }
+
+*/
